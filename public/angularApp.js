36f429003dd4c427d63ac7e3161737adb14c4eed
Mathijs Dumon <mathijs.dumon@gmail.com>
2016-11-28 20:14:07 +0100

    Start of coding the angular side of authentication

--- public/angularApp.js
+++ public/angularApp.js
@@ -2,6 +2,89 @@ var app = angular.module('cadoCom', [
     'ui.router'
 ]);
 
+/*---------------------------------------------------------------------------------------*/
+/*                                     AppConfig                                         */
+/*---------------------------------------------------------------------------------------*/
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+'$httpProvider',
+function($stateProvider, $urlRouterProvider, $httpProvider) {
+
+    var checkLoggedin = function($q, $timeout, $http, $location, $rootScope) {
+        // Initialize a new promise
+        var deferred = $q.defer(); 
+
+        // Check if the user is logged in
+        $http.get('/loggedin').success(function (user) {
+            // Authenticated 
+            if (user !== '0') 
+                deferred.resolve(); 
+            // Not Authenticated 
+            else {
+                $rootScope.message = 'You need to log in.';
+                deferred.reject();
+                $location.url('/login');
+            }
+        });
+
+        return deferred.promise;
+    };  
+
+    /* Detects 401 errors and redirects to login page: */
+    $httpProvider.interceptors.push(
+        function($q, $location) { 
+            return { 
+                response: function(response) { 
+                    // do something on success
+                    return response;
+                },
+                responseError: function(response) {
+                    if (response.status === 401)
+                        $location.url('/login');
+                    return $q.reject(response);
+                }
+            };
+        }
+    );
+
+    $stateProvider
+    .state('home', {
+        url: '/',
+        templateUrl: '/home.html',
+        controller: 'ListCtrl',
+        resolve: {
+            giftPromise: ['gifts', function(gifts) {
+                return gifts.getAll();
+            }],
+            loggedin: checkLoggedin
+        }
+    })
+    .state('gifts', {
+        url: '/gifts/{id}',
+        templateUrl: '/gifts.html',
+        controller: 'GiftsCtrl',
+        resolve: {
+            gift: ['$stateParams', 'gifts', function($stateParams, gifts) {
+              return gifts.get($stateParams.id);
+            }],
+            loggedin: checkLoggedin
+        } 
+    })
+    .state('login', {
+        url: '/login',
+        templateUrl: '/views/login.html',
+        controller: 'LoginCtrl'
+    });
+
+    $urlRouterProvider.otherwise('home');
+
+
+}]);
+
+/*---------------------------------------------------------------------------------------*/
+/*                                REST API Factories                                     */
+/*---------------------------------------------------------------------------------------*/
 app.factory('gifts', ['$http', function($http){
     var o = {
         gifts: []
@@ -24,6 +107,9 @@ app.factory('gifts', ['$http', function($http){
     return o;
 }]);
 
+/*---------------------------------------------------------------------------------------*/
+/*                                    Controllers                                        */
+/*---------------------------------------------------------------------------------------*/
 app.controller('ListCtrl', [
     '$scope',
     'gifts',
@@ -55,33 +141,31 @@ app.controller('GiftsCtrl', [
     }
 ]);
 
-app.config([
-'$stateProvider',
-'$urlRouterProvider',
-function($stateProvider, $urlRouterProvider) {
+app.controller('LoginCtrl', [
+    '$scope',
+    '$rootScope',
+    '$http',
+    '$location',
+    function($scope, $rootScope, $http, $location) {
+        // This object will be filled by the form
+        $scope.user = {};
 
-  $stateProvider
-    .state('home', {
-        url: '/',
-        templateUrl: '/home.html',
-        controller: 'ListCtrl',
-        resolve: {
-            giftPromise: ['gifts', function(gifts) {
-                return gifts.getAll();
-            }]
-        }
-    })
-    .state('gifts', {
-        url: '/gifts/{id}',
-        templateUrl: '/gifts.html',
-        controller: 'GiftsCtrl',
-        resolve: {
-            gift: ['$stateParams', 'gifts', function($stateParams, gifts) {
-              return gifts.get($stateParams.id);
-            }]
-        } 
-    });
-
-    $urlRouterProvider.otherwise('home');
-}]);
-
+        // Register the login() function
+        $scope.login = function () {
+            $http.post('/login', {
+                username: $scope.user.username,
+                password: $scope.user.password,
+            })
+            .success(function(user){
+                // No error: authentication OK
+                $rootScope.message = 'Authentication successful!';
+                $location.url('/admin');
+            })
+            .error(function(){
+                // Error: authentication failed
+                $rootScope.message = 'Authentication failed.';
+                $location.url('/login');
+            });
+        };
+    }
+]);
