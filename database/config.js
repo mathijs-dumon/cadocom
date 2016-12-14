32d72d9a64797aa33351e09552a43be5490247a6
Mathijs Dumon <mathijs.dumon@gmail.com>
2016-12-14 14:42:03 +0100

    Updating database config to work with mLab mongo addon on heroku

--- database/config.js
+++ database/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   'secret': 'cadocomIsNeat',
-  'database': 'mongodb://localhost/cadocom'
+  'database': (process.env.MONGODB_URI!=undefined) ? process.env.MONGODB_URI : 'mongodb://localhost/cadocom'
 };
